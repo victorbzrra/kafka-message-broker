a701b9f55058f1383c00b58753c53782ed675fbe
Raphael3c <phael.ufc@gmail.com>
2023-12-04 08:30:59 -0300

    feat: armazenando as mensagens no banco

--- Consumer-Store-Mongo/index.js
+++ Consumer-Store-Mongo/index.js
@@ -1,11 +1,28 @@
-const TOPIC = "message-thread";
-const kafka = require('kafka-node');
+import Kafka from 'kafka-node';
+import { MongoClient } from "mongodb";
 
-const consumer = new kafka.Consumer(
-  new kafka.KafkaClient({kafkaHost: 'localhost:9092'}),
+const TOPIC = "message-thread";
+const CON_STRING_MONGO = "mongodb+srv://Rapha:1234ufc@mensagens-sd-final.dkwslmf.mongodb.net";
+
+const client = new MongoClient(CON_STRING_MONGO)
+
+const insert = async (message) => {
+  await client.connect();
+
+  const dbMessage = client.db("SD-Final")
+
+  const messageCol = dbMessage.collection('Messages')
+
+  await messageCol.insertOne(message);
+
+  client.close()
+}
+
+const consumer = new Kafka.Consumer(
+  new Kafka.KafkaClient({kafkaHost: 'localhost:9092'}),
   [{ topic: TOPIC }]
 );
 
 consumer.on('message', function (message) {
-  console.log(message.value);
+  insert({...JSON.parse(message.value), dt: new Date()})
 });
